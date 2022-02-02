050ea4f51f5e65fdb8a1111e410d5c810217ce71
David Shin <shin.david93@gmail.com>
2022-02-02 08:01:48 -0800

    Remove empty h6 from StudentPosts

--- src/views/StudentPosts.js
+++ src/views/StudentPosts.js
@@ -8,7 +8,6 @@ export default function StudentPosts(props) {
                         <h1 className="card-title my-2">{props.post.title}</h1>
                         <hr/>
                         <h6 className="card-text">{props.post.body} <span className="fst-italic">{`- ${props.post.user.first_name} ${props.post.user.last_name} (${props.post.id}) `}</span></h6>
-                        <h6 className="card-subtitle mb-3"></h6>
                         <h6 className="card-subtitle text-white-50 fw-light">{props.post.date_created}</h6>
                     </div>
                 </div>
